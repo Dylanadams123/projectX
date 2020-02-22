e95727e0ec2d5feef7051803289e528e6b5f4fd9
Dylan Adams-Hegarty <dylanadams-hegarty@Dylans-MacBook-Pro.local>
2020-02-22 01:21:24 -0500

    First commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,8 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "./bootstrap-grid.min.css"
+import "./bootstrap.min.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
